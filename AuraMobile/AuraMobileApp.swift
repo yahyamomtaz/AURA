b41aa4ef82c2a20c7f8e8014f1b77481fcd37472
yahyamomtaz <yahyamomtaz@gmail.com>
2023-05-10 12:40:33 +0200

    Initial Commit

---
+++ AuraMobile/AuraMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraMobileApp.swift
+//  AuraMobile
+//
+//  Created by Yahya Momtaz on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AuraMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
